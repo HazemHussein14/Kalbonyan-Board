b64a0f74e02b878adbc6c310b3747cab1e044a8b
Hazem Hussein <113997542+HazemHussein14@users.noreply.github.com>
2023-03-07 07:45:17 +0200

    added main content and styled all elements

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const edit = document.querySelector('button')
-const task = document.querySelector('li')
+// const edit = document.getElementById("edit-button");
+// const task = document.querySelector("li");
+// const write = document.querySelector(".write");
 
-edit.addEventListener('click', () => {
-  task.contentEditable = true;
-  task.focus();
-})
+// edit.addEventListener("click", () => {
+//   write.contentEditable = true;
+//   write.focus();
+//   const range = document.createRange();
+//   range.selectNodeContents(write);
+//   range.collapse(false);
+//   const selection = window.getSelection();
+//   selection.removeAllRanges();
+//   selection.addRange(range);
+// });
